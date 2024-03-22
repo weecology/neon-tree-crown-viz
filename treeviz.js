dc1b3a6320ad2bbde637cd0632b32dfb734724a8
Ethan White <ethan@weecology.org>
2024-03-21 21:55:27 -0400

    Move to multi-site visualization
    Start with SJER + OSBS as an example

--- treeviz.js
+++ treeviz.js
@@ -2,116 +2,153 @@
   mapboxgl.accessToken = 'pk.eyJ1IjoiYndlaW5zdGVpbiIsImEiOiJjbHJ3aWV6OXIxM3llMmpsZXNjZjNocDBxIn0.aNJNRY4h_3l6IqyrQirH4A'; // Still figuring out the secure way to handle this
   
   const mapdata = document.querySelector('#map');
-  let rasterTileset = mapdata.dataset.rastertileset;
-  let vectorTileset = mapdata.dataset.vectortileset;
-  let rasterTilesetURL = 'mapbox://bweinstein.' + rasterTileset;
-  let vectorTilesetURL = 'mapbox://bweinstein.' + vectorTileset;
-  let speciesIDs = mapdata.dataset.speciesids;
-  let speciesList = speciesIDs.split(",");
+
+  function makeMap(selectedSite, selectedYear, selectedLocation, selectedSpecies) {
+        // Use the new value
+        let rasterTileset = selectedSite + '_' + selectedYear + '_rgb';
+        let vectorTileset = selectedSite + '_trees';
+        let rasterTilesetURL = 'mapbox://bweinstein.' + rasterTileset;
+        let vectorTilesetURL = 'mapbox://bweinstein.' + vectorTileset;
+    
+        let speciesList = selectedSpecies;
+      
+      //  document.getElementById('rastertileset-select').addEventListener('change', function() {
+      //    document.getElementById('map').dataset.rastertileset = this.value;
+      //  });
+        
+        const map = new mapboxgl.Map({
+          container: 'map', // container id
+          style: 'mapbox://styles/mapbox/light-v11', // stylesheet location
+          center: selectedLocation, // starting position [lng, lat]
+          zoom: 12 // starting zoom
+        });
+      
+        // Define your categories and corresponding colors
+        var num_species = speciesList.length
+        console.log(num_species)
+        var colors = chroma.scale('Spectral').colors(num_species)
+      
+        // Create the match expression for color boxes by species
+        var speciesColors = ['match', ['get', 'sci_name']]; // Replace 'property-name' with the name of your property
+        speciesList.forEach(function(species, i) {
+            speciesColors.push(species, colors[i]);
+        });
+        speciesColors.push('#000'); // Default color
+      
+        map.on('load', () => {
+          map.addSource('raster-source', { // A unique id for the source
+              type: 'raster',
+              url: rasterTilesetURL, // The Mapbox id for the tileset
+              tileSize: 256
+          });
+      
+          map.addLayer({
+              id: 'raster-layer', // A unique id for the layer
+              type: 'raster',
+              source: 'raster-source', // The id of the source to use for this layer
+              paint: {
+                  'raster-opacity': 0.85 // Adjust the opacity of the raster layer
+              }
+          });
+      
+      // To display the bounding boxes as outlines while making them easily clickable
+      // add two layers for trees data. The 'fill' layer makes it easy to click on each item.
+          map.addLayer({
+            id: 'trees',
+            type: 'fill',
+            source: {
+              type: 'vector',
+              url: vectorTilesetURL
+            },
+            'source-layer': selectSite.value + '_webmercator',
+            'paint': {
+              'fill-opacity': 0
+          }
+        });
+      
+        map.addLayer({
+            id: 'trees-outline',
+            type: 'line',
+            source: {
+              type: 'vector',
+              url: vectorTilesetURL
+            },
+            'source-layer': selectSite.value + '_webmercator',
+            'paint': {
+              'line-color': speciesColors,
+              'line-width': 2
+          }
+        });
+      
+        map.on('click', 'trees', (e) => {
+          // Check if features exist
+          if (!e.features || e.features.length === 0) {
+              console.log("No features found");
+              return;
+          }
+      
+          // Check if geometry and coordinates exist
+          if (!e.features[0].geometry || !e.features[0].geometry.coordinates) {
+              console.log("No coordinates found");
+              return;
+          }
+          // Copy coordinates array.
+          const coordinates = e.features[0].geometry.coordinates[0][0].slice();
+      
+          // Check if properties and description exist
+          if (!e.features[0].properties || !e.features[0].properties.sci_name) {
+              console.log("No sci_name found");
+              return;
+          }
+      
+          const dom_score = e.features[0].properties.dom_score;
+          const sci_name = e.features[0].properties.sci_name;
+      
+          new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(sci_name + "<br>" + "Score: " + dom_score + "<br>" + "Lat: " + coordinates[1].toFixed(5) + " " + "Long: " + coordinates[0].toFixed(5))
+            .addTo(map);
+      
+          });
+        });
+      
+          // Change the cursor to a pointer when the mouse is over the places layer.
+        map.on('mouseenter', 'trees', () => {
+        map.getCanvas().style.cursor = 'pointer';
+        });
+        
+        // Change it back to a pointer when it leaves.
+        map.on('mouseleave', 'trees', () => {
+        map.getCanvas().style.cursor = '';
+        });    
+  }
+
+  let siteYearDict = {
+    "SJER": "2021",
+    "OSBS": "2021"
+  }
+
+  let siteLocationDict = {
+    "SJER": [-119.73228, 37.09],
+    "OSBS": [-81.993431, 29.689282]
+  }
   
-  const map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/light-v11', // stylesheet location
-    center: [-119.73228, 37.09], // starting position [lng, lat]
-    zoom: 12 // starting zoom
-  });
+  let siteSpeciesDict = {
+    "SJER": ["Quercus douglasii Hook. & Arn.", "Quercus wislizeni A. DC.", "Pinus sabiniana Douglas ex Douglas"],
+    "OSBS": ["Pinus palustris Mill.", "Pinus elliottii Engelm.", "Pinus clausa (Chapm. ex Engelm.) Vasey ex Sarg.", "Liquidambar styraciflua L.", "Quercus geminata Small", "Quercus virginiana Mill.", "Quercus hemisphaerica W. Bartram ex Willd.", "Pinus taeda L.", "Nyssa sylvatica Marshall", "Acer rubrum L.", "Quercus laurifolia Michx.", "Magnolia sp.", "Quercus nigra L.", "Quercus laevis Walter", "Carya glabra (Mill.) Sweet"]
+  }
 
-  // Define your categories and corresponding colors
-  var num_species = speciesList.length
-  console.log(num_species)
-  var colors = chroma.scale('Spectral').colors(num_species)
+  var selectSite = document.getElementById('site-select');
+  var selectedSite = selectSite.value;
+  var selectedYear = siteYearDict[selectedSite];
+  var selectedLocation = siteLocationDict[selectedSite];
+  var selectedSpecies = siteSpeciesDict[selectedSite];
+  makeMap(selectedSite, selectedYear, selectedLocation, selectedSpecies);
 
-  // Create the match expression for color boxes by species
-  var speciesColors = ['match', ['get', 'dom_taxa']]; // Replace 'property-name' with the name of your property
-  speciesList.forEach(function(species, i) {
-      speciesColors.push(species, colors[i]);
-  });
-  speciesColors.push('#000'); // Default color
-
-  map.on('load', () => {
-    map.addSource('raster-source', { // A unique id for the source
-        type: 'raster',
-        url: rasterTilesetURL, // The Mapbox id for the tileset
-        tileSize: 256
-    });
-
-    map.addLayer({
-        id: 'raster-layer', // A unique id for the layer
-        type: 'raster',
-        source: 'raster-source', // The id of the source to use for this layer
-        paint: {
-            'raster-opacity': 0.85 // Adjust the opacity of the raster layer
-        }
-    });
-
-// To display the bounding boxes as outlines while making them easily clickable
-// add two layers for trees data. The 'fill' layer makes it easy to click on each item.
-    map.addLayer({
-      id: 'trees',
-      type: 'fill',
-      source: {
-        type: 'vector',
-        url: vectorTilesetURL
-      },
-      'source-layer': 'SJER_2021',
-      'paint': {
-        'fill-opacity': 0
-    }
-  });
-
-  map.addLayer({
-      id: 'trees-outline',
-      type: 'line',
-      source: {
-        type: 'vector',
-        url: vectorTilesetURL
-      },
-      'source-layer': 'SJER_2021',
-      'paint': {
-        'line-color': speciesColors,
-        'line-width': 2
-    }
-  });
-
-  map.on('click', 'trees', (e) => {
-    // Check if features exist
-    if (!e.features || e.features.length === 0) {
-        console.log("No features found");
-        return;
-    }
-
-    // Check if geometry and coordinates exist
-    if (!e.features[0].geometry || !e.features[0].geometry.coordinates) {
-        console.log("No coordinates found");
-        return;
-    }
-    // Copy coordinates array.
-    const coordinates = e.features[0].geometry.coordinates[0][0].slice();
-
-    // Check if properties and description exist
-    if (!e.features[0].properties || !e.features[0].properties.dom_taxa) {
-        console.log("No dom_taxa found");
-        return;
-    }
-
-    const dom_taxa = e.features[0].properties.dom_taxa;
-    const dom_score = e.features[0].properties.dom_score;
-    const sci_name = e.features[0].properties.sci_name;
-
-    new mapboxgl.Popup()
-      .setLngLat(coordinates)
-      .setHTML(dom_taxa + "<br>" + "Score: " + dom_score + "<br>" + "Lat: " + coordinates[1].toFixed(5) + " " + "Long: " + coordinates[0].toFixed(5))
-      .addTo(map);
-
-    });
-  });
-
-    // Change the cursor to a pointer when the mouse is over the places layer.
-  map.on('mouseenter', 'trees', () => {
-  map.getCanvas().style.cursor = 'pointer';
-  });
-  
-  // Change it back to a pointer when it leaves.
-  map.on('mouseleave', 'trees', () => {
-  map.getCanvas().style.cursor = '';
+  selectSite.addEventListener('change', function() {
+    var selectedSite = this.value;
+    var selectedYear = siteYearDict[selectedSite];
+    var selectedLocation = siteLocationDict[selectedSite];
+    var selectedSpecies = siteSpeciesDict[selectedSite];
+    makeMap(selectedSite, selectedYear, selectedLocation, selectedSpecies);
   });
